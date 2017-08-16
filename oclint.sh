5ca9f010b45c94883f338a9089cd88ad4f8a3062
hehtao <hehtao@163.com>
2017-08-16 13:32:21 +0800

    Update oclint.sh

--- oclint.sh
+++ oclint.sh
@@ -1,4 +1,15 @@
 # 更多规则请参阅https://github.com/oclint/oclint
+if which oclint 2>/dev/null; then
+echo 'oclint exist'
+else
+brew tap oclint/formulae
+brew install oclint
+fi
+if which xcpretty 2>/dev/null; then
+echo 'xcpretty exist'
+else
+gem install xcpretty
+fi
 xcodebuild clean
 xcodebuild -workspace=grid  -scheme=grid | xcpretty -r json-compilation-database
 cp build/reports/compilation_db.json compile_commands.json
